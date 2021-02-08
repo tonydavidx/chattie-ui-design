a82976393d177eeb58453187ed68b0c6e79081bd
Tony David <tonydavidx@gmail.com>
2021-02-08 18:36:31 +0530

    App Starting

---
+++ lib/app_theme.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyTheme {
+  MyTheme._();
+  static Color kPrimaryColor = Color(0xff7C7B9B);
+  static Color kPrimaryColorVariant = Color(0xff686795);
+  static Color kAccentColor = Color(0xffFCAAAB);
+  static Color kAccentColorVariant = Color(0xffF7A3A2);
+  static Color kUnreadChatBG = Color(0xffEE1D1D);
+
+  static final TextStyle kAppTitle = GoogleFonts.grandHotel(fontSize: 36);
+
+  static final TextStyle heading2 = TextStyle(
+    color: Color(0xff686795),
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    letterSpacing: 1.5,
+  );
+
+  static final TextStyle chatSenderName = TextStyle(
+    color: Colors.white,
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    letterSpacing: 1.5,
+  );
+
+  static final TextStyle bodyText1 = TextStyle(
+      color: Color(0xffAEABC9),
+      fontSize: 14,
+      letterSpacing: 1.2,
+      fontWeight: FontWeight.w500);
+
+  static final TextStyle bodyTextMessage =
+      TextStyle(fontSize: 13, letterSpacing: 1.5, fontWeight: FontWeight.w600);
+
+  static final TextStyle bodyTextTime = TextStyle(
+    color: Color(0xffAEABC9),
+    fontSize: 11,
+    fontWeight: FontWeight.bold,
+    letterSpacing: 1,
+  );
+}
---
+++ lib/models/user_model.dart
@@ -0,0 +1,39 @@
+class User {
+  final int id;
+  final String name;
+  final String avatar;
+
+  User({
+    this.id,
+    this.name,
+    this.avatar,
+  });
+}
+
+final User currentUser =
+    User(id: 0, name: 'You', avatar: 'assets/images/Addison.jpg');
+
+final User addison =
+    User(id: 1, name: 'Addison', avatar: 'assets/images/Addison.jpg');
+
+final User angel =
+    User(id: 2, name: 'Angel', avatar: 'assets/images/Angel.jpg');
+
+final User deanna =
+    User(id: 3, name: 'Deanna', avatar: 'assets/images/Deanna.jpg');
+
+final User jason = User(id: 4, name: 'Json', avatar: 'assets/images/Jason.jpg');
+
+final User judd = User(id: 5, name: 'Judd', avatar: 'assets/images/Judd.jpg');
+
+final User leslie =
+    User(id: 6, name: 'Leslie', avatar: 'assets/images/Leslie.jpg');
+
+final User nathan =
+    User(id: 7, name: 'Nathan', avatar: 'assets/images/Nathan.jpg');
+
+final User stanley =
+    User(id: 8, name: 'Stanley', avatar: 'assets/images/Stanley.jpg');
+
+final User virgil =
+    User(id: 9, name: 'Virgil', avatar: 'assets/images/Virgil.jpg');
